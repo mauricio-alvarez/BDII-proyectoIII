08cbb3e53b70fc5dfb283df51359e66fafc59f84
blackBear <63987435+Ajax-Kroos@users.noreply.github.com>
2023-07-09 11:38:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import face_recognition
-import random,os,math,cv2,time, rtree, pickle, heapq,json,time
+import random,os,math,cv2,time, rtree, pickle, heapq,json,time, heapq
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from rtree.index import Index
 
-def load_encoding(filename, index, encoding_size=128):
+def loadVector(filename, index, encoding_size=128):
     with open(filename, 'rb') as f:
         f.seek(index * encoding_size * 8)
         encoding = np.fromfile(f, dtype=np.float64, count=encoding_size)
     return encoding
-def load_all(filename,total):
+
+def loadNVectors(filename,total):
     data = list()
     for i in range(total):
-        data.append(load_encoding(filename, i))
+        data.append(loadVector(filename, i))
     return data
 
-
-def save_encodings(encodings, filename):
-    with open(filename, 'wb') as f:
-        for encoding in encodings:
-            np.array(encoding).tofile(f)
-
-def guardar_diccionario(diccionario, archivo):
+def saveDict(diccionario, archivo):
     with open(archivo, 'w') as file:
         json.dump(diccionario, file)
 
-def guardarCaracterisiticas(filename):
+def saveCharacteristics(filename):
     path = "C:/Users/Administrador/Desktop/UTEC/BASE DE DATOS 2/lfw"
     folders = os.listdir(path)
     metadataJSON = dict()
@@ -44,11 +40,10 @@ def guardarCaracterisiticas(filename):
                 except :
                     print("Error")
 
-    guardar_diccionario(metadataJSON,filename+'_metadata.txt')
+    saveDict(metadataJSON,filename+'_metadata.txt')
 
-#guardarCaracterisiticas("faceEncodings.bin")
 
-def comparacionPares(comparaciones, vectores):
+def parComparison(comparaciones, vectores):
     resultados = list()
     for i in range(comparaciones):
         num1 = random.randint(0, 13175)
@@ -56,20 +51,70 @@ def comparacionPares(comparaciones, vectores):
         resultados.append(np.linalg.norm(vectores[num1] - vectores[num2]))
     return resultados
 
-def knnSequential(k, query):
-    pass
-def rangeSearch(r, query):
-    pass
+def knnSequential(lista_valores, query, k):
+    heap = []
+    
+    for valor in lista_valores:
+        distancia = np.linalg.norm(valor - query)
+        heapq.heappush(heap, (distancia, valor))
+        
+        # Si la cola de prioridad supera el tamaño K, eliminamos el elemento más lejano
+        if len(heap) > k:
+            heapq.heappop(heap)
+    return heap
+
+def rangeSearch(lista_valores, query, r):
+    result = list()
+    for valor in lista_valores:
+        distanciaE = np.linalg.norm(valor - query)
+        if(distanciaE <= r):
+            result.append(distanciaE,valor)
+
+    return result
+
+def buildRindex(vectors):
+    
+    #1- borrando archivos anteriores
+    if os.path.exists("128-index.data"):
+        os.remove("128-index.data")
+    if os.path.exists("128-index.index"):
+        os.remove("128-index.index") 
+    
+
+    # 2- configurar el indice
+    prop = rtree.index.Property()
+    prop.dimension = 128
+    prop.buffering_capacity = 3 # M
+    prop.dat_extension = "data"
+    prop.idx_extension = "index"
+
+    ind = rtree.index.Index('128-index', properties = prop)
+
+    # 3- insertar los puntos
+    for i in range(len(vectors)):
+        ind.insert(i, vectors[i])
+
+def loadRIndex(filename):
+    prop = rtree.index.Property()
+    prop.dimension = 128
+    prop.buffering_capacity = 3 # M
+    prop.dat_extension = "data"
+    prop.idx_extension = "index"
+    ind = rtree.index.Index("128-index", properties=prop)
+    return ind
 
 
-vectores = load_all("faceEncodings.bin",13176)
 
-#linealScan()
-lista = comparacionPares(15000, vectores)
-# Crear el histograma
-plt.hist(lista, bins=50)
-plt.xlabel('Distancia Euclidiana')
-plt.ylabel('Frecuencia')
-plt.title('Histograma de Distancias Euclidianas 9000')
-plt.show()
+
+#saveCharacteristics("faceEncodings.bin")
+
+vectores = loadNVectors("faceEncodings.bin",50)
+
+#k_vecinos = knnSequential(vectores,vectores[50],2)
+#print(k_vecinos)
+
+buildRindex(vectores)
+loadRIndex()
+
+
 
